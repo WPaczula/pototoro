132e6c8ff5744dc50f76751545ea08ffa4e29500
Wojciech Paczuła <wojciech.paczula@gmail.com>
2020-03-15 23:30:10 +0100

    Updates

---
+++ precache-manifest.63854d0b078a1402b6b5a4f740caaf80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3d8a9de1baa4a041d7bbb6a1e75aa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a91d23a20fc4a9a74c",
+    "url": "/static/css/main.f3be964c.chunk.css"
+  },
+  {
+    "revision": "468ffa1f4e15bbd5fbf5",
+    "url": "/static/js/2.f5e01b80.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f5e01b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a91d23a20fc4a9a74c",
+    "url": "/static/js/main.95c51d44.chunk.js"
+  },
+  {
+    "revision": "ca05b8270289d4bb6a26",
+    "url": "/static/js/runtime-main.04fffb4f.js"
+  }
+]);
